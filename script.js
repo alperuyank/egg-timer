0be06a48833e73cbc2999d76c4b5427da43232c9
alperuyank <109217631+alperuyank@users.noreply.github.com>
2025-02-21 15:46:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let timer;
 let remainingTime = 0;
 let isRunning = false;
-let audio = new Audio("/sounds/default.mp3");
+let audio = new Audio("https://cms-artifacts.motionarray.com/content/motion-array/109678/sfx4_MP3.mp3?Expires=2031957701487&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=i5AP-FmzxD0LAgVEwveLU7BPdbV7J1miuYoQkOKoD~Zd1Zyoo55Vb-vjQ7bedlobLiYTqX6rpTc1K-0goUvhsIqWqZHfMVyinQI3XbJzwR2EgND~5Xe94F6v5dD1c9rI6DWfB6DwnUj0rL-r0nr8JG6z8ngeZsJr0ivzEacCYjCbD62wREEpBiagBTZXseojVMpkS40Esobe6ox3niTds2iSxOq2FwK1aT71E7qZltqdA63fx4UFYFqWu9x1-KN8UxIs3vmAJGKzo9xePGF~HFHMf~iHg8ca~rXoJl~zHg6k3vlDGTMhDAaMnunGhW3Tr91gf~2DGcUKgpXmV3fVyQ__");
 
 function startTimer() {
   if (isRunning) return;
